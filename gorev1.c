b0f9da016832a46cdfaf3972cd0d3f2780f90f47
omerdalar <45cq49gmrh@privaterelay.appleid.com>
2026-07-28 13:48:44 +0300

    Add first C program with comments

---
+++ gorev1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Ekrana yazı yazdırmak için gerekli kütüphane
+
+int main() {
+    // Programın başlangıç noktası
+
+    printf("selam ya");
+    // Ekrana yazdırma
+
+    return 0;
+    // Program hatasız şekilde tamamlandı
+
+}
