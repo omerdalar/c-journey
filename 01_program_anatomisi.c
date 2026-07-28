f283a4dbf26370a0afa604e6ff2efff7cae2aa19
omerdalar <45cq49gmrh@privaterelay.appleid.com>
2026-07-28 14:20:13 +0300

    Add first C program with comments

---
+++ 01_program_anatomisi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Ekrana yazı yazdırmak için gerekli olan kütüphane
+
+int main() {
+    // Programın başlangıç noktası
+
+    printf("selam ya");
+    // Ekrana yazdırma
+
+    return 0;
+    // Program hatasız şekilde tamamlandı
+}
